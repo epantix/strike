541384bc0f8da43ebb8775ba9aa8df1474e0fb0e
Artemiy Dudko <thepioh@zoho.com>
2024-06-28 16:18:12 +0300

    добавили комментарии

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
+// Класс Bullet представляет собой объект пули
 class Bullet {
+  // Конструктор принимает координаты x и y, а также угол a
   constructor(x, y, a) {
+    // Создаем новый элемент div для пули
     this.div = document.createElement("div");
+    // Устанавливаем класс "bullet" для стилизации пули
     this.div.className = "bullet";
+    // Добавляем элемент div в элемент с id "map"
     document.getElementById("map").appendChild(this.div);
 
+    // Устанавливаем начальные координаты и угол пули
     this.x = x;
     this.y = y;
     this.a = a;
+    // Вычисляем начальные скорости пули по осям x и y
     this.vx = Math.sin((this.a * Math.PI) / 180) * 10;
     this.vy = -Math.cos((this.a * Math.PI) / 180) * 10;
+    // Вызываем метод move() для начала движения пули
     this.move();
   }
 
+  // Метод move() обновляет координаты пули и обновляет их на экране
   move() {
+    // Обновляем координаты пули с учетом скоростей по осям x и y
     this.x += this.vx;
     this.y += this.vy;
 
+    // Обновляем позицию элемента div на экране
     this.div.style.left = this.x + "px";
     this.div.style.top = this.y + "px";
   }
 }
 let bullets = [];
 
+/**
+ * Класс Rocket представляет ракету в игре.
+ * @class
+ */
 class Rocket {
+  /**
+   * Создает экземпляр класса Rocket.
+   * @constructor
+   * @param {string} id - Идентификатор элемента ракеты.
+   * @param {number} x - Координата X ракеты.
+   * @param {number} y - Координата Y ракеты.
+   */
   constructor(id, x, y) {
     this.id = id;
     this.div = document.getElementById(id);
@@ -34,6 +56,9 @@ class Rocket {
     this.vy = 0;
   }
 
+  /**
+   * Перемещает ракету на основе текущей скорости.
+   */
   move() {
     let maxSpeed = 20;
     if (this.vx > maxSpeed) this.vx = maxSpeed;
@@ -61,11 +86,18 @@ class Rocket {
     this.div.style.transform = `rotate(${this.a}deg)`;
   }
 
+  /**
+   * Увеличивает или уменьшает скорость ракеты в зависимости от знака.
+   * @param {number} sign - Знак (+1 или -1) для увеличения или уменьшения скорости.
+   */
   gas(sign) {
     this.vx += Math.sin((this.a * Math.PI) / 180) * sign;
     this.vy -= Math.cos((this.a * Math.PI) / 180) * sign;
   }
 
+  /**
+   * Создает и добавляет пулю в массив пуль.
+   */
   fire() {
     let offsetX = 45;
     let offsetY = 25;
